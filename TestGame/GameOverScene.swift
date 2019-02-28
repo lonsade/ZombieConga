074aecdcbc5c308bd3ea6c2187b264c3792dd9a6
Nikita Zhudin <dantes04015380209@gmail.com>
2019-02-28 17:09:42 +0300

    git init

---
+++ TestGame/GameOverScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameOverScene.swift
+//  TestGame
+//
+//  Created by Nikita Zhudin on 28/02/2019.
+//  Copyright © 2019 Nikita Zhudin. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+final class GameOverScene: SKScene {
+    
+}
